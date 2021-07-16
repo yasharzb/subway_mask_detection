dea71ceb3cb8c0edf882e8b2376985d9e0336d3b
yasharzb <yasharzb@ce.sharif.edu>
2021-07-16 11:31:34 +0430

    Increase period time and print python outputs in PythonUtil

--- src/main/java/PythonUtil.java
+++ src/main/java/PythonUtil.java
@@ -19,6 +19,7 @@ public final class PythonUtil {
             Process process = processBuilder.start();
             List<String> results = new BufferedReader(new InputStreamReader(process.getInputStream())).lines()
                     .collect(Collectors.toList());
+            System.err.println((Arrays.toString(results.toArray())));
             int exitCode = process.waitFor();
             assert exitCode == 0;
             return Optional.of(results);
